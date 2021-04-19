623b52dadd3dff71ad4b6a40ff4c484fc618618a
Diogo Godoi <diogo.godoi@usp.br>
2021-04-19 16:03:03 -0300

    Iniciando app

---
+++ Projeto Despesas Pessoais/despesas_pessoais/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:despesas_pessoais/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo celular',
+      value: 1300.00,
+      data: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Roupas',
+      value: 309.90,
+      data: DateTime.now(),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+            ),
+          ),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
+          )
+        ],
+      ),
+    );
+  }
+}
